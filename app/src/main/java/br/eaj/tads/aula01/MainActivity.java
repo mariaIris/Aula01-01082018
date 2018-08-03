327471cf2ac06776c78d08978b28cb4a3eac5d3f
Aluno <airis.vital@hotmail.com>
2018-08-03 11:26:11 -0300

    Aula de PDM - 01/ago

---
+++ app/src/main/java/br/eaj/tads/aula01/MainActivity.java
@@ -0,0 +1,60 @@
+package br.eaj.tads.aula01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView texto;
+    EditText edit;
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {//um dos primeiros metodos do ciclo de vida do android
+        super.onCreate(savedInstanceState);//
+        setContentView(R.layout.activity_main);//minha tela vai ter a view desenhada, essa tela é obrigatória
+
+        texto = findViewById(R.id.ola); //instancia
+        texto.setText("Bom dia!");
+
+        edit = findViewById(R.id.edit);
+        edit.setText("Hello!");
+
+        /*  View.OnClickListener meu_listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String texto_edit = edit.getText().toString();
+                texto.setText(texto_edit);
+            }
+        };
+        Button btLogin = findViewById(R.id.botao);
+        btLogin.setOnClickListener(meu_listener);*/
+
+        Button btLogin = findViewById(R.id.botao);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String texto_edit = edit.getText().toString();
+                texto.setText(texto_edit);
+
+                Toast.makeText(MainActivity.this, texto_edit, Toast.LENGTH_SHORT).show();
+
+                Log.i("batatinha","O texto foi:"+ texto_edit);
+            }
+        });
+
+    }
+
+   /* public void bananinha_clique(View v){
+        EditText edit = findViewById(R.id.edit);
+        String texto_edit = edit.getText().toString();
+
+        TextView texto = findViewById(R.id.ola);
+        texto.setText(texto_edit);
+    */
+}
